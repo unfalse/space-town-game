47516c60f95b3e50b95f131da5954d074804fe15
Nopefish <fadeout01@gmail.com>
2021-05-15 20:12:42 +0500

    added russian section for README; invisibility cheat mode (bots can't detect you); removed useless dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CircularDependencyPlugin = require('circular-dependency-plugin');
 
 module.exports = {
     mode: 'development',
@@ -24,18 +23,7 @@ module.exports = {
         }),
         new HtmlWebpackPlugin({
             template: 'src/index.html',
-        }),
-        new CircularDependencyPlugin({
-            // exclude detection of files based on a RegExp
-            exclude: /a\.js|node_modules/,
-            // add errors to webpack instead of warnings
-            failOnError: true,
-            // allow import cycles that include an asyncronous import,
-            // e.g. via import(/* webpackMode: "weak" */ './file.js')
-            allowAsyncCycles: false,
-            // set the current working directory for displaying module paths
-            cwd: process.cwd(),
-        }),
+        })
     ],
     resolve: {
         extensions: ['.ts', '.js'],
@@ -49,22 +37,6 @@ module.exports = {
             },
         ],
     },
-    // module: {
-    //   rules: [
-    //     {
-    //       test: /\.(ts|js)$/,
-    //       exclude: /node_modules/,
-    //       use: {
-    //         loader: "babel-loader",
-    //         options: {
-    //           presets: [
-    //             "@babel/preset-env",
-    //           ]
-    //         }
-    //       }
-    //     },
-    //   ]
-    // },
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         port: 3666,
